22fe301e64cf98a9e164200eb050ecbb6edc3991
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-05-01 19:23:25 +0300

    Change filename

---
+++ PortCleaner/WinNaTDriverReseter(Admin).bat
@@ -0,0 +1,4 @@
+net stop winnat
+net start winnat
+ECHO Driver successfully reseted! Ports should be available now!
+PAUSE
--- PortCleaner/WinNaTDriverReseter.bat
+++
@@ -1,5 +0,0 @@
-net stop winnat
-TIMEOUT 3
-net stop winnat
-TIMEOUT 3
-ECHO Driver successfully reseted! Ports should be available now!
